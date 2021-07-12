90f0393da85f5d2f42d9e9ee00c25b159b0d5176
catarina6t <catarinatorres@live.com.pt>
2021-07-12 22:35:40 +0100

    Showing the Current Temperature

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
-let apiKey = "10a4c9df0a4335af83e171aa8f6662cd";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
-console.log(apiUrl);
+let apiKey = `5f1f55f9358951a8197b84c8616e01cf`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=porto&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);

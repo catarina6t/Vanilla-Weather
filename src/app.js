ec10a840ba48ccc0bb0d5271cb8d7c9653df0602
catarina6t <catarinatorres@live.com.pt>
2021-07-28 16:24:05 +0100

    Final Changes

--- src/app.js
+++ src/app.js
@@ -48,7 +48,7 @@ function displayForecast(response) {
             forecastDay.weather[0].icon
           }@2x.png"
           alt=""
-          width="42"
+          width="40" id-"weather-image"
         />
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> ${Math.round(
@@ -140,4 +140,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("New York");
+search("Porto");
